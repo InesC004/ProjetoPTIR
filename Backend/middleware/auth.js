2bf217a593e8b70b10411b08779aabd2ef40f277
inessantoscabral <fc62835@alunos.fc.ul.pt>
2026-03-17 23:46:57 +0000

    logiin??

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,16 +1,9 @@
-const { expressjwt: jwt } = require('express-jwt')
-const jwksRsa = require('jwks-rsa')
+const { auth } = require('express-oauth2-jwt-bearer')
 
-const auth = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: 'https://SEU-DOMINIO-AUTH0/.well-known/jwks.json'
-  }),
-  audience: 'SUA-AUDIENCE',
-  issuer: 'https://SEU-DOMINIO-AUTH0/',
-  algorithms: ['RS256']
+const jwtCheck = auth({
+  audience: 'https://takeacab-api',
+  issuerBaseURL: 'https://dev-wgz277zrovymotmr.us.auth0.com/',
+  tokenSigningAlg: 'RS256'
 })
 
-module.exports = auth
+module.exports = jwtCheck
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Auth0Provider
+      domain="dev-wgz277zrovymotmr.us.auth0.com"
+      clientId="Ug8lxW0wDqeofYLblhBrMeUpSA949aSS"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: "https://takeacab-api"
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
 );
