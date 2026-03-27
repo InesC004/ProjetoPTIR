ec74435b5cb0824a54745d99070650898214ece3
tomassetas <fc58952@alunos.fc.ul.pt>
2026-03-27 12:21:29 +0000

    heeeheheh

--- Backend/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-const { auth } = require('express-oauth2-jwt-bearer')
-
-const jwtCheck = auth({
-  audience: 'https://takeacab-api',
-  issuerBaseURL: 'https://dev-wgz277zrovymotmr.us.auth0.com/',
-  tokenSigningAlg: 'RS256'
-})
-
-module.exports = jwtCheck
--- Backend/models/preco.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const precoSchema = new mongoose.Schema({
-  nivel_conforto: { type: String, enum: ['basico','luxuoso'], required: true },
-  preco_minuto: { type: Number, required: true },
-  acrescimo_noturno: { type: Number, default: 0 } 
-})
-
-module.exports = mongoose.model('preco', precoSchema)
